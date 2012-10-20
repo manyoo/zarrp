d435196a04c32d326645ad9897fdc3f6070ae1da
Eric Wong <ericsyw@gmail.com>
2012-10-20 10:00:00 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zarrp::Application.config.secret_token = '2d9f4cd3fe00461a489849f6564e7ffb8267a37c0af0a60715b37b798f8c7d8a38bd17a970b35f69e111a68009cda9b336613cb772e5c594fea6b817e5d2fbe2'
