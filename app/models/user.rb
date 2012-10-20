b1cf69771af623525f7dd1029611cb6963a80650
Eric Wong <ericsyw@gmail.com>
2012-10-20 15:10:19 +0800

    Fix a typo in User model. Add basic User tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
   validates :name, :presence => true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, :presence => true, :format => { :with => VALID_EMAIL_REGEX }
-  validates :access_token, :presence => true, :uniqeness => true
+  validates :access_token, :presence => true, :uniqueness => true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,58 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before { @user = User.new(name: "test", email: "test@example.com", access_token: "alkdlafalkdsflksdfjlakdh") }
+    
+    subject { @user }
+
+    it { should respond_to(:name) }
+    it { should respond_to(:email) }
+    it { should respond_to(:access_token) }
+    it { should respond_to(:registrations) }
+    it { should respond_to(:events) }
+
+    describe "when user name is not present" do
+        before { @user.name = '' }
+        it { should_not be_valid }
+    end
+
+    describe "when user email is not present" do
+        before { @user.email = '' }
+        it { should_not be_valid }
+    end
+
+    describe "when access_token is not present" do
+        before { @user.access_token = '' }
+        it { should_not be_valid }
+    end
+
+    describe "when email format is invalid" do
+        it "should be invalid" do
+            addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                           foo@bar_baz.com foo@bar+baz.com]
+            addresses.each do |invalid_addr|
+                @user.email = invalid_addr
+                @user.should_not be_valid
+            end
+        end
+    end
+
+    describe "when email format is valid" do
+        it "should be valid" do
+            addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+            addresses.each do |valid_addr|
+                @user.email = valid_addr
+                @user.should be_valid
+            end
+        end
+    end
+
+    describe "when user access_token is not unique" do
+        before {
+            @user_with_same_token = @user.dup
+            @user_with_same_token.save
+        }
+
+        it { should_not be_valid }
+    end
 end
